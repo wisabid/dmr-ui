ed226cc79afa86852cfb1964f7b4963a7d1b071d
wisabid <abidwsi@yahoo.com>
2022-05-03 10:55:08 +0100

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header style={{padding: '20px 0'}}>
-            <h1 style={{color: '#fff'}}>Dataminr Dashboard</h1>
+            <h1 style={{color: '#fff'}}>Dmr Dashboard</h1>
         </header>
     )
 }
